b7614a4be7da8a2ed07caca111e3a0e226796768
Kazuaki Ueda <kazuaki.ueda@gmail.com>
2023-11-17 10:07:10 +0900

    first commit.

---
+++ functions.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Functions
+ *
+ * @package    WordPress
+ */
+
+function my_theme_enqueue_styles()
+{
+    $parenthandle = 'parent-style';
+    $theme        = wp_get_theme();
+    wp_enqueue_style(
+        $parenthandle,
+        get_template_directory_uri() . '/style.css',
+        array(),
+        $theme->parent()->get('Version')
+    );
+    wp_enqueue_style(
+        'child-style',
+        get_stylesheet_uri(),
+        array( $parenthandle ),
+        $theme->get('Version')
+    );
+}
+add_action('wp_enqueue_scripts', 'my_theme_enqueue_styles', 11);
